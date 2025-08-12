5ba3b3a7d4ca04dfb3c3bad017d74bb797b44909
valientjuno <jessedoake91@gmail.com>
2025-08-12 12:33:49 -0600

    console displays articles

--- news-header.js
+++ news-header.js
@@ -1,5 +1,23 @@
-const Url =
-  "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3662fe9fb0054975b5d1bb6c61efdece";
+const newsApiKey = "662fe9fb0054975b5d1bb6c61efdece";
+const country = "us";
+const category = "technology";
 
-fetch(Url);
-console.log(Url);
+const Url =
+  "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=3662fe9fb0054975b5d1bb6c61efdece";
+
+fetch(Url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json(); // Parse the JSON response
+  })
+  .then((data) => {
+    console.log(data.articles);
+
+    // Access the news articles
+    // Process and display the news data here
+  })
+  .catch((error) => {
+    console.error("Error fetching news:", error);
+  });
