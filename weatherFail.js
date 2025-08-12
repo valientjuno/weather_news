6e487aa1089b2e564972ae5879578f6de98f8686
valientjuno <jessedoake91@gmail.com>
2025-08-12 10:09:14 -0600

    ok

--- weatherFail.js
+++ weatherFail.js
@@ -49,29 +49,29 @@
 //     }
 //   });
 
-// const apiKey = "Ybc805f89e83cdacb25b45ea6feb8b0ee"; // Replace with your actual API key
-// const city = "Tooele"; // Example city
-// const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=tooele&&units=imperial&appid=bc805f89e83cdacb25b45ea6feb8b0ee`;
+const apiKey = "Ybc805f89e83cdacb25b45ea6feb8b0ee"; // Replace with your actual API key
+const city = "Tooele"; // Example city
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=tooele&&units=imperial&appid=bc805f89e83cdacb25b45ea6feb8b0ee`;
 
-// fetch(apiUrl)
-//   .then((response) => {
+fetch(apiUrl)
+  .then((response) => {
 
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
 
-//     }
+    }
 
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log("Weather data:", data);
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Weather data:", data);
 
-//     const temperature = data.main.temp;
-//     console.log(`Temperature in ${city}: ${temperature}°F`);
+    const temperature = data.main.temp;
+    console.log(`Temperature in ${city}: ${temperature}°F`);
 
-//   })
+  })
 
-//   .catch((error) => {
-//     console.error("Error fetching weather data:", error);
+  .catch((error) => {
+    console.error("Error fetching weather data:", error);
 
-//   });
+  });
